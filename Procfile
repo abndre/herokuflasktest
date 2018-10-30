12678e5eff1d5ec8790611e4d06450f3b5d44d56
Andre <andrezio@live.com>
2018-10-30 20:54:02 -0300

    update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: FLASK_APP=main.py -m flask run --host=0.0.0.0 --port=$PORT
+web: FLASK_APP=main.py -m flask run 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 
 app = Flask(__name__)
 
@@ -6,3 +8,7 @@ app = Flask(__name__)
 def hello_world():
     return 'HELLO'
 
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
