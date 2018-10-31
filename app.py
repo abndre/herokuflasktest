755eafb0574412c0b61383b241d48680ee64ae18
Andre <andrezio@live.com>
2018-10-31 07:44:46 -0300

    update

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from flask import jsonify
 
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+
+
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import os
 
 
